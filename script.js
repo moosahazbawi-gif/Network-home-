3523a423102ae28213a79ffebc52b485b86d4e70
moosahazbawi-gif <moosahazbawi@gmail.com>
2026-06-18 15:15:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ function sendToWhatsApp() {
   let phone = document.getElementById("phone").value;
   let service = document.getElementById("service").value;
   let message = document.getElementById("message").value;
+let text =
+`درخواست جدید سرویس:
+👤 نام و نام خانوادگی: ${name}
+📱 شماره تماس: ${phone}
+🛠 خدمات انتخابی: ${service}
+📝 جزئیات درخواست: ${message}`;
 
-  let text =
-`طلب خدمة جديد:
-👤 الاسم: ${name}
-📱 الهاتف: ${phone}
-🛠 الخدمة: ${service}
-📝 التفاصيل: ${message}`;
-
-  let url = "https://wa.me/989000000000?text=" + encodeURIComponent(text);
+  let url = "https://wa.me/+989050780371?text=" + encodeURIComponent(text);
 
   window.open(url, "_blank");
 }
